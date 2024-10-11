914a7f18abbcf23d9e74939527c0d54e70549ec5
Christos S <sidchristos_2@hotmail.com>
2024-10-11 16:05:17 +0300

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,63 @@
-import { useState } from 'react'
-import './App.css'
+// eslint-disable-next-line no-unused-vars
+import React,  {lazy, Suspense}  from 'react'
 import Header from'./components/Header'
 import MainScrn from'./components/MainScrn'
+import Error from "./components/Error"
 import Footer from './components/Footer'
+import axios from "axios";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './output.css'
 
-function App() {
+const SinglePost = lazy(() => import("./components/SinglePost"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:
+    <Suspense fallback={<Loading />}>
+      <MainScrn />,
+    </Suspense>,
+    errorElement: <Error />,
+  },
+  {
+    path: "/error",
+    element: <Error />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/SinglePost/:id",
+    loader: async ({ params }) => {
+      const response = await axios.get(
+        `http://localhost:3000/post/${params.id}`
+      );
+      return response;
+    },
+    element: 
+      <Suspense fallback={<Loading />}>
+        <SinglePost />,
+      </Suspense>,
+    errorElement: <Error />,
+  },
+]);
+
+
+export default function App(){
 
   return (
     <>
-      <div className="App">
-          <Header></Header>
-          <MainScrn></MainScrn>
-          <Footer></Footer> 
-      </div>
+      <Suspense fallback={<Loading />}>
+        <Header/>
+        <RouterProvider router={router} />
+        <Footer/>
+      </Suspense>
     </>
-  )
-}
+    );
+  }
 
-export default App
+function Loading() {
+  return (
+    <h2 className="w-screen h-screen flex justify-center items-center font-[700] text-[64px] text-black text-left tracking-tight capitalize">
+      🌀 Loading...
+    </h2>
+  );
+}  
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,35 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import '../App.css';
 
 export default function Header(){
     return (
-        <header>
-                 <div className="site-name">MySite</div>
-                 <div className="nav-links">
-                    <a href="#Page"className="nav-link">Page</a>
-                    <a href="#Page"className="nav-link">Page</a>
-                    <a href="#Page"className="nav-link">Page</a>
-                    <button className="nav-button">Button</button>
+        <>
+            <div className="py-14 px-12 md:p-12 lg:p-15  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 lg:gap-10 w-6/6">
+                <div className="md:col-span-1  lg:col-span-3" id="Right">
+                    <div className="py-4 lg:py-0 md:py-0  flex flex-col">
+                    <h1 className="text-xl font-bold text-black tracking-tight">
+                        <a className="text-black font-[500] text-[20px] inline-block " href="/"> Site Name </a>
+                    </h1>
+                    </div>
                 </div>
-        </header>
+                <div className="col-span-2  lg:col-span-2 " id="Left">
+                    <div className="flex flex-col tracking-tight">
+                    <ul className="flex flex-row justify-start md:justify-end lg:justify-end gap-5 md:gap-6 lg:gap-7 items-center">
+                        <li>
+                        <a href="/" className="hover:font-bold font-[500] text-[20px] cursor-pointer text-black">Page</a>
+                        </li>
+                        <li>
+                        <a href="/" className="hover:font-bold font-[500] text-[20px] cursor-pointer text-black">Page</a>
+                        </li>
+                        <li>
+                        <a href="/" className="hover:font-bold font-[500] text-[20px] cursor-pointer text-black">Page</a>
+                        </li>
+                        <button className="nav-button">Button</button>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
