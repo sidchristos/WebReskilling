1b65f0f8656ed59e1f374ced7d8413dfa0747b51
Christos <sidchristos_2@hotmail.com>
2024-10-14 10:19:41 +0300

    Quick fix CSS

--- Frontend/src/components/IntroPost.jsx
+++ Frontend/src/components/IntroPost.jsx
@@ -13,16 +13,16 @@ export default function IntroPost({ intro }){
                 <h4 id="line2" className="text-[#787777] text-[24px] font-[400] tracking-tight">{intro.IntroLine2}</h4>
                 <img id="img1" className="pt-10 rounded-lg" src={intro.photo}></img>
             </div>
-            <div className="flex flex-wrap gap-8  lg:px-52 md:px-32  text-black text-l text-[20px] font-[500] tracking-tight w-11/12 h-full">
+            <div className="flex flex-wrap gap-8  lg:px-52 md:px-32  text-black text-l text-[20px] font-[500] tracking-tight w-11/12 h-full lg:ml-20">
                 <p id="par1" className=' 2xl:ml-40 '> {intro.paragraph1} </p>
                 <p id="par2" className=' 2xl:ml-40'> {intro.paragraph2} </p>
                 <p id="par3" className=' 2xl:ml-40'> {intro.paragraph3} </p>
             </div>
-            <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-rows-1 gap-8 lg:h-dvh w-full ">
+            <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-rows-1 gap-8 lg:h-dvh w-full -mb-20">
                 <img id="img2" className="rounded-lg  md:h-5/6 lg:h-5/6 sm:h-auto w-full " src={intro.photo2}></img>
                 <img id="img3" className="rounded-lg  md:h-5/6 lg:h-5/6 sm:h-auto w-full " src={intro.photo3}></img>
             </div>
-            <div className="flex flex-col gap-6 lg:pb-12 lg:px-52 md:px-32 text-black text-[20px] font-[500] tracking-tight w-10/12 h-full -mt-11">
+            <div className="flex flex-col gap-6 lg:pb-12 lg:px-52 md:px-32 text-black text-[20px] font-[500] tracking-tight w-10/12 h-full -mt-11 lg:ml-20">
                 <p id="par4" className=' 2xl:ml-40'> {intro.paragraph4} </p>
                 <p id="par5" className=' 2xl:ml-40'> {intro.paragraph5} </p>
             </div>
